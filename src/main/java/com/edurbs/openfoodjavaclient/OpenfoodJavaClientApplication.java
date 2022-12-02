d22f22d9fe9deb5824a008b818f25c86686b9c3b
Eduardo Soares <edurbs@gmail.com>
2022-12-02 18:01:07 -0300

    start

---
+++ src/main/java/com/edurbs/openfoodjavaclient/OpenfoodJavaClientApplication.java
@@ -0,0 +1,13 @@
+package com.edurbs.openfoodjavaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenfoodJavaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenfoodJavaClientApplication.class, args);
+	}
+
+}
